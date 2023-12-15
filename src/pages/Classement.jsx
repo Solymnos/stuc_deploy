eca5fca597460d33726a76e0ca7a742076567941
Solymnos <113204065+Solymnos@users.noreply.github.com>
2023-12-15 19:13:44 +0100

    change src of classement

--- src/pages/Classement.jsx
+++ src/pages/Classement.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import TopBar from '../components/TopBar'
-import { Bracket } from 'react-brackets';
+import { SingleEliminationBracket, Match } from '@g-loot/react-tournament-brackets';
 
 let teams  = 
 [
@@ -15,47 +15,156 @@ let teams  =
   { 'name' : 'SLYP', 'icon' : './img/logo/SLYP.png', 'pts' : '19', 'pts_quizz' : '4', 'pts_ftn' : '6', 'pts_ssbu' : '4', 'pts_tft' : '1', 'pts_rl' : '4'},
 ]
 
-let rounds = [
+const matches = [
   {
-    title: '1/4 Finale',
-    seeds: [
+    "id": 19753,
+    "nextMatchId": null,
+    "tournamentRoundText": "3",
+    "startTime": "2021-05-30",
+    "state": "SCHEDULED",
+    "participants": []
+  },
+  {
+    "id": 19754,
+    "nextMatchId": 19753,
+    "tournamentRoundText": "2",
+    "participants": [
       {
-        id: 1,
-        teams: [{ name: 'EXODIA', score : '3' }, { name: 'GG CALL DOG', score : '0' }],
+        "id": "14754a1a-932c-4992-8dec-f7f94a339960",
+        "resultText": "3",
+        "isWinner": true,
+        "status": "PLAYED",
+        "name": "EXODIA",
+        "picture": "teamlogos/client_team_default_logo"
       },
       {
-        id: 2,
-        teams: [{ name: 'CALECON FREEGUN' }, { name: 'POLARY' }],
-      },
-      {
-        id: 3,
-        teams: [{ name: 'UNO' }, { name: 'NULARY' }],
-      },
-      {
-        id: 4,
-        teams: [{ name: 'LOS AUTISTO 93' }, { name: 'GG CALL DRAKE' }],
+        "id": "d8b9f00a-0ffa-4527-8316-da701894768e",
+        "resultText": "0",
+        "isWinner": false,
+        "status": null,
+        "name": "CALECON FREEGUN",
+        "picture": "teamlogos/client_team_default_logo"
       }
-    ],
+    ]
   },
   {
-    title: '1/2 Finale',
-    seeds: [
+    "id": 19755,
+    "nextMatchId": 19754,
+    "tournamentRoundText": "1",
+    "participants": [
       {
-        id: 5,
-        teams: [{ name: 'EXODIA' }, { name: 'CALECON FREEGUN' }],
+        "id": "14754a1a-932c-4992-8dec-f7f94a339960",
+        "resultText": "3",
+        "isWinner": true,
+        "status": "PLAYED",
+        "name": "EXODIA",
+        "picture": "teamlogos/client_team_default_logo"
       },
       {
-        id: 6,
-        teams: [{ name: 'NULARY' }, { name: 'LOS AUTISTO 93' }],
-      },
-    ],
+        "id": "d16315d4-7f2d-427b-ae75-63a1ae82c0a8",
+        "resultText": "0",
+        "isWinner": false,
+        "status": "PLAYED",
+        "name": "GG CALL DOG",
+        "picture": "teamlogos/client_team_default_logo"
+      }
+    ]
   },
   {
-    title : 'Finale',
-    seeds: [
+    "id": 19756,
+    "nextMatchId": 19754,
+    "tournamentRoundText": "1",
+    "state": "",
+    "participants": [
       {
-        id:7,
-        teams: [{name: '???'}, {name: '???'}]
+        "id": "d8b9f00a-0ffa-4527-8316-da701894768e",
+        "resultText": "3",
+        "isWinner": true,
+        "status": null,
+        "name": "CALECON FREEGUN",
+        "picture": "teamlogos/client_team_default_logo"
+      },
+      {
+        "id": "d8b9f00a-0ffa-4527-8316-da701894768f",
+        "resultText": "2",
+        "isWinner": false,
+        "status": null,
+        "name": "POLARY",
+        "picture": "teamlogos/client_team_default_logo"
+      },
+    ]
+  },
+  {
+    "id": 19757,
+    "nextMatchId": 19753,
+    "tournamentRoundText": "2",
+    "state": "",
+    "participants": [
+      {
+        "id": "9397971f-4b2f-44eb-a094-722eb286c59b",
+        "resultText": "",
+        "isWinner": false,
+        "status": null,
+        "name": "NULARY",
+        "picture": "teamlogos/client_team_default_logo"
+      },
+      {
+        "id": "42fecd89-dc83-4821-80d3-718acb50a30c",
+        "resultText": "",
+        "isWinner": false,
+        "status": null,
+        "name": "LOS AUTISTO 93",
+        "picture": "teamlogos/client_team_default_logo"
+      }
+    ]
+  },
+  {
+    "id": 19758,
+    "nextMatchId": 19757,
+    "tournamentRoundText": "1",
+    "startTime": "2021-05-30",
+    "state": "SCHEDULED",
+    "participants": [
+      {
+        "id": "9397971f-4b2f-44eb-a094-722eb286c59b",
+        "resultText": "0",
+        "isWinner": false,
+        "status": null,
+        "name": "UNO",
+        "picture": "teamlogos/client_team_default_logo"
+      },
+      {
+        "id": "9397971f-4b2f-44eb-a094-722eb286c59c",
+        "resultText": "3",
+        "isWinner": true,
+        "status": null,
+        "name": "NULARY",
+        "picture": "teamlogos/client_team_default_logo"
+      }
+    ]
+  },
+  {
+    "id": 19759,
+    "nextMatchId": 19757,
+    "tournamentRoundText": "1",
+    "startTime": "",
+    "state": "",
+    "participants": [
+      {
+        "id": "42fecd89-dc83-4821-80d3-718acb50a30c",
+        "resultText": "3",
+        "isWinner": true,
+        "status": null,
+        "name": "LOS AUTISTO 93",
+        "picture": "teamlogos/client_team_default_logo"
+      },
+      {
+        "id": "df01fe2c-18db-4190-9f9e-aa63364128fe",
+        "resultText": "0",
+        "isWinner": false,
+        "status": null,
+        "name": "GG CALL DRAKE",
+        "picture": "/img/logo/DDCDR.png"
       }
     ]
   }
@@ -149,7 +258,7 @@ function Classement() {
           ) : (
           <div className='flex justify-center items-center'>
             <div>
-              <Bracket rounds={rounds} />
+              <SingleEliminationBracket matches={matches} matchComponent={Match}/>
             </div>
           </div>
             
